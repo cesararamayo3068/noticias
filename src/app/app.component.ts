fe60896d5780895990cba5215dad0dfb9c95357c
Cesar Aramayo <cesar_aramayo@hotmail.es>
2021-12-26 20:52:40 -0300

    consumiendo datos de la api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NoticiaService } from './services/noticia.service';
 
 @Component({
   selector: 'app-root',
@@ -7,7 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'noticias';
+constructor(private _noticiaService :NoticiaService){
 
+}
 
 buscarNoticias(parametros:any){
  console.log('soy el padre');
--- src/app/services/noticia.service.ts
+++ src/app/services/noticia.service.ts
@@ -1,9 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NoticiaService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  getNoticias(parametros:any):Observable<any>{
+    const URL='https://newsapi.org/v2/top-headlines?country='
+    + parametros.pais +'&category='+ parametros.categoria +'&apiKey=1d42a6a2ec304279bb194551f0923345'
+  return this.http.get(URL);
+  }
 }

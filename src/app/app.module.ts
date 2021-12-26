f56b4b1c4e0ecfd488428405f34a1e046788b13c
Cesar Aramayo <cesar_aramayo@hotmail.es>
2021-12-26 18:20:29 -0300

    created component spinner

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { FormularioComponent } from './components/formulario/formulario.component';
 import { ListadoNoticiasComponent } from './components/listado-noticias/listado-noticias.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { SpinnerComponent } from './shared/spinner/spinner.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FormularioComponent,
     ListadoNoticiasComponent,
-    NavbarComponent
+    NavbarComponent,
+    SpinnerComponent
   ],
   imports: [
     BrowserModule,

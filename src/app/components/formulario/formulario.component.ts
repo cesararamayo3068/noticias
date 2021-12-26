dd4aa4e8ddbbf61dd8c83791615c5f345872ce0a
Cesar Aramayo <cesar_aramayo@hotmail.es>
2021-12-26 19:47:23 -0300

    consumiendo datos de los paises y los tipos de noticias dinamicamente desde un array

--- src/app/components/formulario/formulario.component.ts
+++ src/app/components/formulario/formulario.component.ts
@@ -6,6 +6,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./formulario.component.css']
 })
 export class FormularioComponent implements OnInit {
+categorias:any[]=[
+  {value:'general' , nombre:'General'},
+  {value:'business' , nombre:'Negocios'},
+  {value:'entertainment' , nombre:'Entretenimientos'},
+  {value:'health' , nombre:'Salud'},
+  {value:'science' , nombre:'Ciencia'},
+  {value:'sports' , nombre:'Deportes'},
+  {value:'technology' , nombre:'Tecnologia'},
+];
+paises:any[]=[
+  {value:'ar',nombre:'Argentina'},
+  {value:'br',nombre:'Brasil'},
+  {value:'fr',nombre:'Francia'},
+  {value:'hu',nombre:'Hungria'},
+  {value:'mx',nombre:'Mexico'},
+  {value:'gb',nombre:'Reino Unido'},
+
+
+
+
+
+]; 
 
   constructor() { }
 

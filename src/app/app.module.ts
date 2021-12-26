3d809636d0bd35381e0612bc373f054c73fe4e43
Cesar Aramayo <cesar_aramayo@hotmail.es>
2021-12-26 18:17:27 -0300

    creando componente listado de noticias

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormularioComponent } from './components/formulario/formulario.component';
+import { ListadoNoticiasComponent } from './components/listado-noticias/listado-noticias.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FormularioComponent
+    FormularioComponent,
+    ListadoNoticiasComponent
   ],
   imports: [
     BrowserModule,
